343451213260a2fff1037e096bedac1a774aebb1
Fabricio Hang <saveasfabri@gmail.com>
2022-08-27 08:34:25 -0300

    :sparkles: Add: task exercises

--- src/components/TukiChild.js
+++ src/components/TukiChild.js
@@ -3,8 +3,8 @@ import Button from '@mui/material/Button';
 
 
 const TukiChild = () => {
-  //Nuestro primer hook es el useState, el cual nos permite crear un estado en nuestro componente.
-  // let contador = 0;
+  
+  /* Ejercicio botón incrementa y botón decrementa */
   const [count, setCount] = useState(0);
   const increase = () => {
     setCount(count + 1);
@@ -13,11 +13,34 @@ const TukiChild = () => {
     setCount(count - 1);
   };
 
-  const [info,setInfo]=useState({name:'Fabri', Age:43});
+  /* Ejercicio Botón agrega y cambia  datos a objeto info */
+  const [info,setInfo]=useState({name:'Fabri', Age:42});
   console.log('Info', info);
   const changeInfo = () => {
     setInfo ({...info, name:'Fabricio', lastName:'Hang'})
   }
+  const infoInDom = (Object.values(info)).join(', ');
+
+  /* Ejercicio botón cambia loading cada vez que se clickea */
+  const [loading,setLoading]=useState(false);
+  console.log(loading);
+  const changeLoading = () => {
+    setLoading (!loading);
+  }
+
+  /* Ejercicio bot[on agrega animales al array] */
+  const [animals, setAnimals] = useState(['Raccoon']);
+  const arrayAnimals = ['bear', 'eagle', 'rhino', 'cat', 'dog'];
+  console.log('Animals', animals);
+  const addAnimals = () => {
+  /* Así validamos que una vez que clickeamos el boton agrgegará el
+  segundo array sólo si hay un solo elemento en el array. La primera vez
+  sí tiene un solo elemnto('Racoon') entonces lo agrega. Los siguientes 
+   clicks no continuará agregando ya que va a tener 6 elementos */
+  animals.length === 1 && setAnimals([...animals, ...arrayAnimals]);
+  }
+  const arrayAnimalsInDom = ([...animals]).join(', ')
+
 
   return (
     <div className="TukiChild">
@@ -30,9 +53,17 @@ const TukiChild = () => {
           <Button className="btn" onClick={decrease} variant="contained">Click and decrease</Button>
         </div>
       </div>
-      <div className="info">
-      <p>Info: <span>{info}</span> enjoy!</p>
+      <div className="contExercise">
+      <p>Info: <span>{infoInDom}</span></p>
       <Button className="btn" onClick={changeInfo} variant="contained">Change info</Button>
+      </div>
+      <div className="contExercise">
+      <p>Loading: <span>{String(loading)}</span></p>
+      <Button className="btn" onClick={changeLoading} variant="contained">Change Loading</Button>
+      </div>
+      <div className="contExercise">
+      <p>Animals: <span>{arrayAnimalsInDom}</span></p>
+      <Button className="btn" onClick={addAnimals} variant="contained">Add animals</Button>
       </div>
     </div>
   );
